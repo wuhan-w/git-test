da68a79c1e9731ddd4040e7d333df59e24b09dc1
wh <2667537035@qq.com>
2022-08-06 21:41:56 +0800

    push commit

--- src/main/java/it/GitTest.java
+++ src/main/java/it/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("test git in idea");
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("push test");
     }
 }
